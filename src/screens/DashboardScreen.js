edd15a27ca034070dec4a3db96baecceed4a7395
SaschaSchneiders <info@solvbox.com>
2025-10-07 01:17:04 +0200

    Finale! Bärchentour!

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {
   View,
+  Text,
   StyleSheet,
   ScrollView,
+  TouchableOpacity,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
+import { Ionicons } from '@expo/vector-icons';
 import RateScale from '../shared/RateScale';
 import ChartCard from '../shared/ChartCard';
 import RatingBreakdown from '../shared/RatingBreakdown';
